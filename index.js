986e249323f33d48fa7be1aee7f95e371122270b
mrrsmburu <mrrsmburu@gmail.com>
2023-04-05 13:51:49 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 function scuberGreetingForFeet(){
-  // Write your code here!
+  function suberGreetingForFeet(distanceInFeet) {
+    if (distanceInFeet <=) {
+      return "I'm sorry, you must enter a distance greater than zero.";
+    } else if (distanceInFeet <= 1320) {
+      return "You're going to walk " + distanceInFeet + " feet? That's not too far. Have a nice walk!";
+    } else if (distanceInFeet <= 5280) {
+      return "You want to go " + distanceInFeet + " feet? That's about a mile. Let me call you an Uber.";
+    } else {
+      return "You're going to travel " + distanceInFeet + " feet? That's quite a distance! Let me call you a helicopter.";
+    }
+  }
+  
 }
 
 function ternaryCheckCity(){
-  // Write your code here!
+
 }
 
 function switchOnCharmFromTip(){
