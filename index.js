d066b909a1c96ebec9057f3e49aeebbd5a19c6d6
mrrsmburu <mrrsmburu@gmail.com>
2023-10-05 22:11:34 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 function scuberGreetingForFeet(){
-  function suberGreetingForFeet(distanceInFeet) {
-    if (distanceInFeet <=) {
-      return "I'm sorry, you must enter a distance greater than zero.";
-    } else if (distanceInFeet <= 1320) {
-      return "You're going to walk " + distanceInFeet + " feet? That's not too far. Have a nice walk!";
-    } else if (distanceInFeet <= 5280) {
-      return "You want to go " + distanceInFeet + " feet? That's about a mile. Let me call you an Uber.";
-    } else {
-      return "You're going to travel " + distanceInFeet + " feet? That's quite a distance! Let me call you a helicopter.";
+  function scuberGreetingForFeet(distanceInFeet) {
+    if (feet<=400){
+      return 'This one is on me!'
     }
+     else if (feet>2000 && feet<2500) {
+      return "I will gladly take your thirty bucks."
+    }
+     else {
+    return 'No can do.'   
   }
   
 }
@@ -18,12 +17,12 @@ function ternaryCheckCity(){
 }
 
 function switchOnCharmFromTip(){
-  switch (tip) {
+  switch (generous) {
     case 'generous':
-      return 'Thank you so much.';
+      return 'Thank you so much.'
     case 'not as generous':
-      return 'Thank you.';
+      return 'Thank you.'
     default:
-      return 'Bye.';
+      return 'Bye.'
   }
 }
